d8f49a135907ebc1c69dc215762e507ea4a4136c
Mixail <ssmitss58@gmail.com>
2025-07-06 13:34:48 +0300

    feat: add payment and 2 sussces request

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,68 +1,61 @@
-import express, { Request, Response } from 'express';
-import cors from 'cors';
-import {ICreatePayment, YooCheckout} from '@a2seven/yoo-checkout';
+import express, { Request, Response } from "express";
+import cors from "cors";
 
-const database: any = {
+import { ICreatePayment, YooCheckout } from "@a2seven/yoo-checkout";
 
+
+const database ={
+
+    
 }
 
-const app = express();
-const PORT = Number(process.env.PORT) || 4001;
 
-app.use(cors({
-    credentials: true,
-    origin: 'http://5.35.28.88:5173'
-}));
+const app = express();
+const PORT = process.env.PORT || 4001;
+
+app.use(cors());
 app.use(express.json());
 
-// ---------------------------------
+//...............
 
 const YouKassa = new YooCheckout({
-    shopId: '1082575',
-    secretKey: 'test_eoCC7M0gupEbxLPkH2JEr6FyhccP_T_LatFk-0hxvgc'
+  shopId: "1120024",
+  secretKey: "test_kUtBoheJcWDx9cq2AvE588rLUQVkeROgV28-49IEjrM",
 });
 
-app.post('/api/payment', async (req: Request, res: Response) => {
-    const createPayload: ICreatePayment = {
-        amount: {
-            value: req.body.value,
-            currency: 'RUB'
-        },
-        payment_method_data: {
-            type: 'bank_card'
-        },
-        capture: true,
-        confirmation: {
-            type: 'redirect',
-            return_url: 'https://secondly-worthy-shrimp.cloudpub.ru'
-        },
-        metadata: {
-            orderId: req.body.orderId,
-            userId: req.body.userId,
-        }
-    };
+app.post("/api/payment", async (req: Request, res: Response) => {
+  const createPayload: ICreatePayment = {
+    amount: {
+      value: req.body.value,
+      currency: "RUB",
+    },
+    payment_method_data: {
+      type: "bank_card", // тип банковскуая карта
+    },
+    confirmation: {
+      type: "redirect",
+      return_url: "https://github.com/Michael-developerr",
+    },
+    metadata: {
+      orderId: req.body.orderId,
+      userId: req.body.userId,
+    },
+  };
 
-    try {
-        const payment = await YouKassa.createPayment(
-            createPayload,
-            // Не делать так в проде
-            Date.now().toString()
-        );
+  try {
+    const payment = await YouKassa.createPayment(
+      createPayload,
 
-        database[payment.id] = payment;
-        res.json({payment})
-    } catch (error) {
-        console.error(error);
-        res.status(400).json({error: 'error'});
-    }
+      Date.now().toString()
+    ); // тут мы можем указать вместо  Date.now().toString() //  V4 UUID.
+    res.json({ payment });
+    console.log(payment);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ error: "error" });
+  }
 });
 
-app.post('/api/payment/notifications', async (req: Request, res: Response) => {
-    console.log(req.body)
-    database[req.body.id] = req.body;
-    res.json({status: "OK"})
-})
-
-app.listen(PORT, '0.0.0.0' ,() => {
-    console.log(`Server running at http://localhost:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
 });
